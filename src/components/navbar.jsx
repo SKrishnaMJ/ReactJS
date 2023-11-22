de286420c1ba60e6195a8ce77dcdd018bcf27945
SKrishnaMJ <146481712+SKrishnaMJ@users.noreply.github.com>
2023-11-21 23:57:32 -0600

    first commit

---
+++ src/components/navbar.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+//Stateless Functional Component
+const NavBar = ({totalCounters}) => {
+    return (<nav className ="navbar bg-body-tertiary">
+        <div className ="container-fluid">
+          <a className ="navbar-brand" href="#">Navbar <span className='badge badge-pill badge-secondary'>{totalCounters}</span></a>
+        </div>
+      </nav>);
+};
+ 
+export default NavBar;
